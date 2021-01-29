02225697c05b56399883dc72c97315da001ff60a
honly <736956589@qq.com>
2021-01-29 17:22:28 +0800

    添加model&network

--- app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.java
+++ app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.java
@@ -1,4 +1,33 @@
 package com.example.sunnyweather.logic.model;
 
 public class PlaceResponse {
+    String status;
+    Place place;
+
+    public PlaceResponse(String status, Place place) {
+        this.status = status;
+        this.place = place;
+    }
+
+    public static class Place {
+        String name;
+        Location location;
+        String address;
+
+        public Place(String name, Location location, String address) {
+            this.name = name;
+            this.location = location;
+            this.address = address;
+        }
+    }
+
+    public static class Location {
+        String longitude;
+        String latitude;
+
+        public Location(String longitude, String latitude) {
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+    }
 }
