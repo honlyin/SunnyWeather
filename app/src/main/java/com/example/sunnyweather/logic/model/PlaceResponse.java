a1099953926df3187ff7f0e474f0629488f12f17
honly <736956589@qq.com>
2021-01-30 11:29:08 +0800

    [PlaceRespnse]:添加get 与set

--- app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.java
+++ app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.java
@@ -8,8 +8,19 @@ public class PlaceResponse {
     public String status;
     public List<Place> places;
 
-    public PlaceResponse(String status, List<Place> places) {
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
         this.status = status;
+    }
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Place> places) {
         this.places = places;
     }
 
@@ -19,9 +30,27 @@ public class PlaceResponse {
         @SerializedName(value = "formatted_address")
         String address;
 
-        public Place(String name, Location location, String address) {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
             this.name = name;
+        }
+
+        public Location getLocation() {
+            return location;
+        }
+
+        public void setLocation(Location location) {
             this.location = location;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
             this.address = address;
         }
     }
@@ -30,8 +59,19 @@ public class PlaceResponse {
         String longitude;
         String latitude;
 
-        public Location(String longitude, String latitude) {
+        public String getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(String longitude) {
             this.longitude = longitude;
+        }
+
+        public String getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(String latitude) {
             this.latitude = latitude;
         }
     }
